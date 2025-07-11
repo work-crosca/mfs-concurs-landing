4e4a025753d6798245b8540547b216da5e7997a9
FESK0 <work.crosca@gmail.com>
2025-07-11 14:38:56 +0300

    change card section

--- client/src/components/AboutCardSection.jsx
+++ client/src/components/AboutCardSection.jsx
@@ -1,6 +1,6 @@
 import React, { useRef } from "react";
 import { useTranslation } from "react-i18next";
-import { motion, useInView } from "framer-motion";
+import { motion, useInView, useScroll, useTransform } from "framer-motion";
 import "../styles/AboutCardSection.css";
 import cardImage from "../assets/home/card.png?w=800&format=webp&as=src";
 
@@ -9,22 +9,36 @@ const AboutCardSection = () => {
 
   const imageRef = useRef(null);
   const textRef = useRef(null);
+  const containerRef = useRef(null);
+
   const imageInView = useInView(imageRef, { once: true, amount: 0.6 });
   const textInView = useInView(textRef, { once: true, amount: 0.6 });
 
+  // scale on scroll
+  const { scrollYProgress } = useScroll({
+    target: containerRef,
+    offset: ["start end", "end start"]
+  });
+  const scale = useTransform(scrollYProgress, [0, 1], [1, 1.1]);
+
   return (
-    <section className="about-card-container">
+    <section className="about-card-container" ref={containerRef}>
       <motion.div
         className="about-card-image"
         ref={imageRef}
-        initial={{ opacity: 0, scale: 0.9 }}
-        animate={imageInView ? { opacity: 1, scale: 1 } : {}}
+        initial={{ opacity: 0, scale: 0.8}}
+        animate={imageInView ? { opacity: 1, scale: 1.1 } : {}}
         transition={{ duration: 0.6 }}
       >
-        <img
+        <motion.img
           src={cardImage}
           alt="Moldcell Visa Card"
           loading="lazy"
+          style={{
+            scale,
+            y: ["0%", "-2%", "0%", "2%", "0%"], // plutire
+            transition: "y 4s ease-in-out infinite"
+          }}
         />
       </motion.div>
 
